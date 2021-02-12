8985327331c85653deec7da8caf8023598015a5b
adelinel <adeline.latruwe@gmail.com>
2021-02-12 12:25:11 -0800

    added NavBar tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe(`App component`, () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
---
+++ src/NavBar/NavBar.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NavBar from "./NavBar";
+import renderer from "react-test-renderer";
+
+describe(`NavBar component`, () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<NavBar />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders the UI as expected", () => {
+    const tree = renderer.create(<NavBar />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
