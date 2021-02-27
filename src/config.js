7bc2652c97487da84e00f83725200c15abadc7a9
adelinel <adeline.latruwe@gmail.com>
2021-02-27 11:22:18 -0800

    updated for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  IMAGE_URL: "http://localhost:8000/",
+  PORT: process.env.PORT || 8000,
+  API_ENDPOINT:
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
+  IMAGE_URL: process.env.REACT_APP_IMAGE_URL,
   TOKEN_KEY: "FBL-client-auth-token",
 };
