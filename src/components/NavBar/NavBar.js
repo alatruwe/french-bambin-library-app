0399b73c09b495d4f6f8acba4fd917632cf3b7e7
adelinel <adeline.latruwe@gmail.com>
2021-03-01 12:19:14 -0800

    added responsive navbar design

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./NavBar.css";
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+  }
+
+  // navbar toggle on and off
+  toggleClass = () => {
+    this.state.active
+      ? this.setState({ active: false })
+      : this.setState({ active: true });
+  };
+
   renderLogoutNav() {
     return (
-      <div>
-        <Link to="/home">Home</Link>
+      <div className={this.state.active ? "active" : "main-nav"}>
+        <Link to="/home" className="nav-links">
+          Home
+        </Link>
         <br />
-        <Link to="/add-item">Add new item</Link>
+        <Link to="/add-item" className="nav-links">
+          Add new item
+        </Link>
         <br />
-        <Link to="/item-history">Items history</Link>
+        <Link to="/item-history" className="nav-links">
+          Items history
+        </Link>
         <br />
-        <Link onClick={this.props.handleLogOut} to="/">
+        <Link onClick={this.props.handleLogOut} to="/" className="nav-links">
           Logout
         </Link>
         <br />
@@ -23,26 +43,35 @@ class NavBar extends Component {
 
   renderLoginNav() {
     return (
-      <div>
-        <Link to="/signup">Sign Up</Link>
+      <div className={this.state.active ? "active" : "main-nav"}>
+        <Link to="/signup" className="nav-links">
+          Sign Up
+        </Link>
         <br />
         <Link
           to={{
             pathname: `/login`,
             demo: true,
           }}
+          className="nav-links"
         >
           Demo
         </Link>
         <br />
-        <Link to="/login">Log in</Link>
+        <Link to="/login" className="nav-links">
+          Log in
+        </Link>
       </div>
     );
   }
 
   render() {
     return (
-      <nav>
+      <nav className="navbar">
+        <span className="navbar-toggle" onClick={this.toggleClass}>
+          <i className="fas fa-bars"></i>
+        </span>
+        <div className="logo">logo</div>
         {this.props.auth ? this.renderLogoutNav() : this.renderLoginNav()}
       </nav>
     );
