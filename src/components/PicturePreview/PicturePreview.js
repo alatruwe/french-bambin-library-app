7c6e5c2f6cf63d13d516efc4c5ead381f2b8354e
adelinel <adeline.latruwe@gmail.com>
2021-03-04 11:41:48 -0800

    cleaned unused code

--- src/components/PicturePreview/PicturePreview.js
+++
@@ -1,8 +0,0 @@
-export default function PicturePreview(props) {
-  return (
-    <>
-      <img id="output" width="200" src={props.src} />
-      <button onClick={props.delete}>x</button>
-    </>
-  );
-}
